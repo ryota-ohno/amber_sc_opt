3bd406325ca2f6648175b4d684d13b8fd7557452
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2026-06-05 22:00:34 +0900

    Add files via upload

---
+++ tBu_BTBT_Cn/test_3/job.py
@@ -0,0 +1,46 @@
+##tetracene層内計算
+import os
+os.environ['HOME'] ='/data/group1/z40145w'
+import pandas as pd
+import argparse
+import subprocess
+import numpy as np
+
+def init_process(args):
+    auto_dir = f'/home/ohno/Working/amber_sc_opt/tBu_BTBT_Cn/{args.auto_dir}'
+    monomer_name=args.monomer_name
+    df_init=pd.read_csv(os.path.join(auto_dir,'step1_init_params.csv'))
+    os.chdir(auto_dir)
+    job_lines=[
+    '#$ -S /bin/sh \n',
+    '#$ -cwd \n',
+    '#$ -V \n',
+    '#$ -q gr1.q \n',
+    '#$ -pe OpenMP 40 \n',
+    '\n',
+    'hostname \n',
+    '\n',
+    f'python /home/ohno/Working/amber_sc_opt/tBu_BTBT_Cn/src/step1_8_xyz_0.py --auto-dir {args.auto_dir} --monomer-name {monomer_name} --num-nodes 10\n',
+    '\n',
+    '#sleep 12 \n'
+        ]
+    with open(os.path.join(auto_dir,f'job.sh'),'w')as f:
+        f.writelines(job_lines)
+    subprocess.run(['qsub',os.path.join(auto_dir,f'job.sh')])
+
+def update_value_in_df(df,index,key,value):
+    df.loc[index,key]=value
+    return df
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--isTest',action='store_true')
+    parser.add_argument('--auto-dir',type=str,help='path to dir which includes gaussian, gaussview and csv')
+    parser.add_argument('--monomer-name',type=str,help='name of monomer to be calculated')
+    ##maxnum-machine2 がない
+    args = parser.parse_args()
+
+    print("----main process----")
+    init_process(args)
+    print("----finish process----")    
