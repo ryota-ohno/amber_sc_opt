ae33f57674b57487952db2e2eef701234df13652
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2026-06-08 00:35:53 +0900

    Add files via upload

---
+++ tBu_BTBT_Cn/alkyl_pack_1/result.py
@@ -0,0 +1,38 @@
+##tetracene層内計算
+import os
+os.environ['HOME'] ='/data/group1/z40145w'
+import pandas as pd
+import argparse
+import subprocess
+import numpy as np
+
+def result_process(args):
+    subprocess.run(['rm','*.sh.*'])
+    auto_dir = f'/home/ohno/Working/amber_sc_opt/tBu_BTBT_Cn/{args.auto_dir}'
+    df_init=pd.read_csv(os.path.join(auto_dir,'step1_init_params.csv'))
+    df_tot=[]
+    z_list=[np.round(z,1) for z in np.linspace(-4.0,4.0,41)]
+    for z in z_list:
+        dir_name = f'{z}'
+        path_dir=os.path.join(auto_dir,f'{dir_name}')
+        df=pd.read_csv(os.path.join(path_dir,'step1.csv'))
+        df_tot.append(df)
+    df_=pd.concat(df_tot, ignore_index=True)
+    df_.to_csv(os.path.join(auto_dir,'step1.csv'),index=False)
+    
+def update_value_in_df(df,index,key,value):
+    df.loc[index,key]=value
+    return df
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--isTest',action='store_true')
+    parser.add_argument('--auto-dir',type=str,help='path to dir which includes gaussian, gaussview and csv')
+    ##maxnum-machine2 がない
+    args = parser.parse_args()
+
+    print("----main process----")
+    result_process(args)
+    print("----finish process----")
+    
